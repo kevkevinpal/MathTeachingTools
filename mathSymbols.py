3fa3a5ff3dad6fdfe51390ab80176a16b0d39df9
kevkevinpal <oapallikunnel@gmail.com>
2020-05-03 11:35:28 -0500

    added the thereExists symbol scene

--- mathSymbols.py
+++ mathSymbols.py
@@ -9,6 +9,16 @@ import math
 
 class thereExists(Scene):
 	def construct(self):
-		question = TextMobject("What does $\\exists$ mean?")
-		thereExistsSymbol = TextMobject("$\\exists$")
+		question = TextMobject("What does $\\mathbb{Z}$ mean?")
 		self.play(FadeIn(question))
+		self.wait(3)
+		self.play(FadeOut(question))
+		awnser = TextMobject("It is the set of any whole number (ie no fractions allowed)")
+		number_line = NumberLine(include_numbers=True)
+		awnser.next_to(number_line, UP)
+		awnser_animation = [
+			FadeIn(awnser),
+			FadeIn(number_line)
+			]
+		self.play(*awnser_animation)
+		self.wait(4)

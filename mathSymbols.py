72add5c3e18442582708f59b2067bb06d0cd1cf6
Kevin Pallikunnel <kevkevin@Kevins-MBP.attlocal.net>
2020-05-08 18:11:05 -0500

    added another scene for the reals but im struggling to get the decimals to show up on the number line

--- mathSymbols.py
+++ mathSymbols.py
@@ -7,7 +7,7 @@ import math
 
 
 
-class thereExists(Scene):
+class integers(Scene):
 	def construct(self):
 		question = TextMobject("What does $\\mathbb{Z}$ mean?")
 		self.play(FadeIn(question))
@@ -22,3 +22,21 @@ class thereExists(Scene):
 			]
 		self.play(*awnser_animation)
 		self.wait(4)
+
+class reals(Scene):
+    def construct(self):
+                question = TextMobject("What does $\\mathbb{R}$ mean?")
+                self.play(FadeIn(question))
+                self.wait(3)
+                self.play(FadeOut(question))
+                awnser = TextMobject("It is the set of any real number (ie decimals allowed)")
+                numbers_to_show = [-5,-4.5,-4,-3.5,-3,-2.5]
+                number_line = NumberLine(tick_frequency=.1,numbers_to_show=numbers_to_show, include_numbers=True)
+                awnser.next_to(number_line, UP)
+                awnser_animation = [
+                        FadeIn(awnser),
+                        FadeIn(number_line)
+                        ]
+                self.play(*awnser_animation)
+                self.wait(4)
+
